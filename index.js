197f4017193bce1c1583eee570001887e9fc83df
crawmag <crawford.magen@gmail.com>
2022-05-29 11:12:24 -0500

    fixed navbar for mobile view

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const myModal = document.getElementById("myModal");
+const myImageModal = document.getElementById("myImageModal");
 const img = document.getElementsByClassName("merch");
 const modalImg = document.getElementById("img01");
 const caption = document.getElementById("caption");
@@ -7,7 +7,7 @@ const caption = document.getElementById("caption");
 
 for (i = 0; i < img.length; i++) {
     img[i].onclick = function () {
-        myModal.style.display = "block";
+        myImageModal.style.display = "block";
         modalImg.src = this.src;
         caption.innerHTML = this.alt;
     }
@@ -16,5 +16,44 @@ for (i = 0; i < img.length; i++) {
 const span = document.getElementsByClassName("close")[0];
 // function to close modal
 span.onclick = function () {
-    myModal.style.display = "none";
+    myImageModal.style.display = "none";
+}
+
+//get modal
+const modal = document.getElementById("myModal");
+//get navbar and set into modal
+const navbar = document.querySelector(".nav-btn");
+//accessing the links
+const linking = document.querySelectorAll(".linkage");
+//modal is set to default of not open
+let navOpen = false;
+const openModal = () => {
+    if (!navOpen) {
+        navbar.classList.add('open');
+        navOpen = true;
+        modal.style.display = "block"
+    } else {
+        modal.style.display = "none";
+    }
+};
+
+//closing modal once link is clicked
+for(i=0;i< linking.length;i++){    
+    linking[i].onclick = function(){
+    modal.style.display = "none";
+    navbar.classList.remove('open');
+    navOpen = false;
+ }
+}
+
+navbar.addEventListener('click', openModal);
+
+//closing modal once clicked outside of modal
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        navbar.classList.remove('open');
+        navOpen = false;
+        myModal.style.display = "none";
+    }
 }
--- javascript/nav.js
+++ javascript/nav.js
@@ -5,4 +5,43 @@ fetch('../html/nav.html')
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
+
+    //get modal
+const modal = document.getElementById("myModal");
+//get navbar and set into modal
+const navbar = document.querySelector(".nav-btn");
+//accessing the links
+const linking = document.querySelectorAll(".linkage");
+//modal is set to default of not open
+let navOpen = false;
+const openModal = () => {
+    if (!navOpen) {
+        navbar.classList.add('open');
+        navOpen = true;
+        modal.style.display = "block"
+    } else {
+        modal.style.display = "none";
+    }
+};
+
+//closing modal once link is clicked
+for(i=0;i< linking.length;i++){    
+    linking[i].onclick = function(){
+    modal.style.display = "none";
+    navbar.classList.remove('open');
+    navOpen = false;
+ }
+}
+
+navbar.addEventListener('click', openModal);
+
+//closing modal once clicked outside of modal
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        navbar.classList.remove('open');
+        navOpen = false;
+        myModal.style.display = "none";
+    }
+}
 })
